278ad53fdb85bcd60711242d60c3c18777bd465b
AlvarInstitut <alvar@ieselcaminas.org>
2021-02-13 12:37:26 +0100

    Tot menys

--- Existdb_Accedir/app/src/main/java/com/example/exist_db_accedir/MainActivity.kt
+++ Existdb_Accedir/app/src/main/java/com/example/exist_db_accedir/MainActivity.kt
@@ -3,10 +3,9 @@ package com.example.exist_db_accedir
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import net.xqj.exist.ExistXQDataSource
-import java.awt.font.TextAttribute
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,9 +22,8 @@ class MainActivity : AppCompatActivity() {
             val conn = s.getConnection()
             println("Connexió feta")
             val sent = "//ruta/nom"
-
-            val cons = conn.createExpression()
-            val rs = cons.executeQuery(sent)
+            val cons = conn.prepareExpression (sent)
+            val rs = cons.executeQuery ()
 
             while (rs.next())
                 claus?.add(rs.getItemAsString(null))
@@ -36,9 +34,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        System.setProperty("android.javax.xml.stream.XMLInputFactory", "com.sun.xml.stream.ZephyrParserFactory")
-        System.setProperty("android.javax.xml.stream.XMLOutputFactory", "com.sun.xml.stream.ZephyrWriterFactory")
-        System.setProperty("android.javax.xml.stream.XMLEventFactory", "com.sun.xml.stream.events.ZephyrEventFactory")
+        //System.setProperty("android.javax.xml.stream.XMLInputFactory", "com.sun.xml.stream.ZephyrParserFactory")
+        //System.setProperty("android.javax.xml.stream.XMLOutputFactory", "com.sun.xml.stream.ZephyrWriterFactory")
+        //System.setProperty("android.javax.xml.stream.XMLEventFactory", "com.sun.xml.stream.events.ZephyrEventFactory")
         // Des de la versió 3 d'Android, no es permet obrir una connexió des del thread principal.
         // Per tant s'ha de crear un nou.
         sqlThread.start()
--- Redis_Accedir/app/src/main/java/com/example/redis_accedir/MainActivity.kt
+++ Redis_Accedir/app/src/main/java/com/example/redis_accedir/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.redis_accedir
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,17 +13,17 @@ import java.awt.font.TextAttribute
 class MainActivity : AppCompatActivity() {
 
     internal var claus: List<String>? = null
+    val con = Jedis("89.36.214.106")
 
 
     private var sqlThread: Thread = object : Thread() {
         override fun run() {
 
-            val con = Jedis("89.36.214.106")
             con.connect()
             con.auth("ieselcaminas.ad")
 
             claus = con.keys("*").sorted()
-            con.close()
+            //con.close()
 
 
         }
@@ -45,5 +47,47 @@ class MainActivity : AppCompatActivity() {
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, claus!!)
         spinner.adapter = adapter
 
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                var altreThread: Thread = object : Thread() {
+                    override fun run() {
+                        area.setText("")
+                        println("Aiiii" + position)
+                        val clau = claus?.get(position)
+                        println("Aiiii" + clau)
+                        val tip = con.type(clau)
+                        when (tip){
+                            "string" -> area.setText(con.get(clau))
+                            "hash" -> {
+                                for (camp in con.hkeys(clau))
+                                    area.append(camp + " -> " + con.hget(clau,camp) + "\n")
+                            }
+                            "list" -> {
+                                for (camp in con.lrange(clau,0,-1))
+                                    area.append(camp + "\n")
+                            }
+                            "set" -> {
+                                for (camp in con.smembers(clau))
+                                    area.append(camp + "\n")
+                            }
+                            "zset" -> {
+                                for (camp in con.zrevrangeWithScores(clau,0,-1))
+                                    area.append(camp.element + " -> " + camp.score + "\n")
+                            }
+                        }
+
+                    }
+                }
+                altreThread.start()
+                altreThread.join()
+            }
+        }
+
     }
+
+
 }
